350657bf56258891907218abe0ff6dbcbd51ad09
Edward Lindsey <eddielindsey23@gmail.com>
2020-03-09 23:12:45 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 #INPUTS -->Starting number of seconds from midnight --> Integer
 #OUTPUTS --> "HAPPY NEW YEARS"--> String
-#ACTIONS --> puts out number of seconds left for 
+#ACTIONS --> puts out number of seconds left for each 
 
 def countdown
 end
