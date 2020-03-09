cad7304321eb0b26c4436886e2cfbe5ac897ce84
Edward Lindsey <eddielindsey23@gmail.com>
2020-03-09 23:25:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 #write your code here
 
 #INPUTS -->Starting number of seconds from midnight --> Integer
-#OUTPUTS --> "HAPPY NEW YEAR!"--> String
+#OUTPUTS --> "HAPPY NEW YEAR"--> String
 #ACTIONS --> puts out number of seconds left for each loop --> puts a string 
 
 def countdown(seconds)
